e0b57cb19255d5204ce1ea10b95ffcc64f6733cb
Andreas Ingeholm <andreas.ingeholm@gmail.com>
2023-09-01 15:25:38 +0200

    Finished design for now.
    - Added ArticleBody.
    - Added Article.

---
+++ Crito/Views/Partials/_ArticleBody.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Crito.Views.Partials
+{
+    public class _ArticleBodyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
