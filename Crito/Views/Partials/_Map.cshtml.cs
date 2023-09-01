aeca67eb31c27b46ee88141b258343792474483b
Andreas Ingeholm <andreas.ingeholm@gmail.com>
2023-09-01 09:55:06 +0200

    Added breadcrumb. Added placeholders for pages.

---
+++ Crito/Views/Partials/_Map.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Crito.Views.Partials
+{
+    public class _MapModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
